ccaedd22da9fced97438cf98a80b6ab83afac244
nyorain <nyorain@gmail.com>
2017-01-19 22:48:48 +0100

    fix dynVec file doc

--- nytl/dynVec.hpp
+++ nytl/dynVec.hpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 
-/// \file A Vector implementation class.
+/// \file Dynamic-sized specialization for nytl::Vec.
 
 #pragma once
 
